510ec68380af86f0e05d720fc4414be016afc097
Gleb Gorelov <g.gorelov@haulmont.com>
2023-05-23 12:57:59 +0400

    Finished

--- src/main/java/com/company/jmixpm/screen/project/ProjectEdit.java
+++ src/main/java/com/company/jmixpm/screen/project/ProjectEdit.java
@@ -1,10 +1,27 @@
 package com.company.jmixpm.screen.project;
 
+import com.company.jmixpm.entity.User;
+import io.jmix.core.security.CurrentAuthentication;
+import io.jmix.core.usersubstitution.CurrentUserSubstitution;
 import io.jmix.ui.screen.*;
 import com.company.jmixpm.entity.Project;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Project.edit")
 @UiDescriptor("project-edit.xml")
 @EditedEntityContainer("projectDc")
 public class ProjectEdit extends StandardEditor<Project> {
+
+    @Autowired
+    private CurrentUserSubstitution currentUserSubstitution;
+
+//    @Autowired
+//    private CurrentAuthentication currentAuthentication;
+
+    @Subscribe
+    public void onInitEntity(InitEntityEvent<Project> event) {
+//        User user = (User) currentAuthentication.getUser();
+        User user = (User) currentUserSubstitution.getEffectiveUser();
+        event.getEntity().setManager(user);
+    }
 }
---
+++ src/main/java/com/company/jmixpm/security/ProjectManagerRole.java
@@ -0,0 +1,41 @@
+package com.company.jmixpm.security;
+
+import com.company.jmixpm.entity.Document;
+import com.company.jmixpm.entity.Project;
+import com.company.jmixpm.entity.Task;
+import com.company.jmixpm.entity.User;
+import io.jmix.security.model.EntityAttributePolicyAction;
+import io.jmix.security.model.EntityPolicyAction;
+import io.jmix.security.role.annotation.EntityAttributePolicy;
+import io.jmix.security.role.annotation.EntityPolicy;
+import io.jmix.security.role.annotation.ResourceRole;
+import io.jmix.securityui.role.annotation.MenuPolicy;
+import io.jmix.securityui.role.annotation.ScreenPolicy;
+
+import javax.annotation.Nonnull;
+
+@Nonnull
+@ResourceRole(name = "ProjectManager", code = "project-manager")
+public interface ProjectManagerRole {
+
+    @EntityAttributePolicy(entityClass = Project.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
+    @EntityPolicy(entityClass = Project.class, actions = EntityPolicyAction.ALL)
+    void project();
+
+    @EntityAttributePolicy(entityClass = Task.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
+    @EntityPolicy(entityClass = Task.class, actions = EntityPolicyAction.ALL)
+    void task();
+
+    @EntityAttributePolicy(entityClass = Document.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
+    @EntityPolicy(entityClass = Document.class, actions = EntityPolicyAction.ALL)
+    void document();
+
+    @EntityAttributePolicy(entityClass = User.class, attributes = {"firstName", "lastName"}, action = EntityAttributePolicyAction.MODIFY)
+    @EntityAttributePolicy(entityClass = User.class, attributes = {"id", "version", "username", "password", "email"}, action = EntityAttributePolicyAction.VIEW)
+    @EntityPolicy(entityClass = User.class, actions = {EntityPolicyAction.READ, EntityPolicyAction.UPDATE})
+    void user();
+
+    @MenuPolicy(menuIds = {"User.browse", "Project.browse", "Task_.browse", "Document.browse"})
+    @ScreenPolicy(screenIds = {"User.browse", "Project.browse", "Task_.browse", "Document.browse", "Document.edit", "Project.edit", "Task_.edit", "User.edit"})
+    void screens();
+}
